cabce0adbf9005529d3eb1a3b71a3b6cc346e25c
Olga Sh <olgashkarat@outlook.com>
2019-06-15 21:51:01 +0300

    CustomTableViewCell

---
+++ TableViewCourse/CustomTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  CustomTableViewCell.swift
+//  TableViewCourse
+//
+//  Created by Оля on 15/06/2019.
+//  Copyright © 2019 Оля. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    
+}
