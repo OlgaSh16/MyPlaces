447323d76e7c890dfdfea30ad681f470ff6d32be
Olga Sh <olgashkarat@outlook.com>
2019-06-15 20:19:04 +0300

    Initial Commit

---
+++ TableViewCourse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewCourse
+//
+//  Created by Оля on 15/06/2019.
+//  Copyright © 2019 Оля. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
